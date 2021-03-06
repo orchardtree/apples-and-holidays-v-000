a0e1300d2afd673285b61a6b21c681c8bbb333aa
Jesse Wenger <wengerj@gmail.com>
2021-03-06 02:46:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -184,8 +184,8 @@ def all_holidays_with_bbq(holiday_hash)
   # }
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      supplies
-      binding.pry
+      supplies.detect do |
+      end
     end
   end
 end
